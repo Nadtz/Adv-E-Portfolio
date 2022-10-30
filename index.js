d02b0ac61251b4de539c0a3d9dada2586d4608d6
nadtz <nadzirahmustapa257@gmail.com>
2022-10-30 23:57:43 +0800

    javascript created

--- index.js
+++ index.js
@@ -4,15 +4,29 @@
 
  /* async */ function contact(event) {
     event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"         // adding a class always add a space
     /* await */ emailjs
     .sendForm(
-        'service_m6r0lzm',
-        'template_wigd9m1',
-        event.target,
-        'rsPrK5AtL2d9Ax1_E'
-    ).then(() => { /* need to wait for the email server, so add .then() */
-        console.log('this worked')
-    }) /* dont have to use then() if use async/await */
-}
+    'service_m6r0lzm',
+    'template_wigd9m1',
+     event.target,
+     'rsPrK5AtL2d9Ax1_E'
+     ).then(() => {                                         /* need to wait for the email server, so add .then() */
+     loading.classList.remove("modal__overlay--visible");
+     success.classList += " modal__overlay--visible";
+     }).catch(() => {
+        loading.classList.remove("modal__overlay--visible")
+        alert(
+            "The email.service is temporarily unavailable. Please contact me directly at nadzzrahim@gmail.com."
+        );
+     })                                                      /* dont have to use then() if use async/await */
 
-const loading = document.querySelector('.modal__overlay--loading')
+
+// setTimeout(() => {
+ 
+//    console.log('IT WORKED')
+//    }, 1000);
+
+    }
